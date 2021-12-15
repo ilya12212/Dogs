39d0057d653368b32101330828c9d5d0f2222dbf
ILuhA <95561265+ilya12212@users.noreply.github.com>
2021-12-15 20:18:10 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("ilyt");
+        Shepherd shepherd = new Shepherd("Reks",10,3.9,"Average");
+                Dog dog = new Dog("Maks",3,2.5);
+                Bulldog bulldog = new Bulldog("Robert",5,4.2,"English Bulldog");
+                System.out.println(dog);
+                dog.Gav();
+                System.out.println(bulldog);
+                bulldog.Gav();
+                System.out.println(shepherd);
+                shepherd.Gav();
     }
 }
